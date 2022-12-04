281d4cbec5a3197b9eb20f71bfb4e594c2f58d87
a.bobylev <alexey.bobylev@flant.com>
2022-12-04 22:41:10 +0300

    Добавлен раздел в админ сайт

--- project_cards/cards/admin.py
+++ project_cards/cards/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Card, Stack, Group
+from .models import Card, Stack, Group, Chapter
 
 
 admin.site.register(Card)
 admin.site.register(Stack)
 admin.site.register(Group)
+admin.site.register(Chapter)
